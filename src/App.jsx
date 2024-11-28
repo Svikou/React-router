cc777545fa25ea75b7202bc67fdbcf635ff3833e
Svikou <svikoubah2k03@gmail.com>
2024-11-28 12:30:37 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,7 @@ import ProfileCard from "./Components/ProfileCard";
 function App() {
   return (
     <div className="flex h-screen">
-      {/* Barre latérale */}
       <Sidebar />
-
-      {/* Section de droite pour la carte de profil */}
       <div className="flex-1 flex justify-center items-center bg-gray-50">
         <ProfileCard />
       </div>
